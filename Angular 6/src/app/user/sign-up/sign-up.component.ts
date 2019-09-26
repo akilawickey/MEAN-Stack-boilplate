ca3d7806613d0a94e0dc682a0f34df37e3cd9911
Akila_09405 <asak999@gmail.com>
2019-09-26 10:53:34 +0530

    build

--- Angular 6/src/app/user/sign-up/sign-up.component.ts
+++ Angular 6/src/app/user/sign-up/sign-up.component.ts
@@ -1,48 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
-import { UserService } from '../../shared/user.service'
+import { UserService } from "../../shared/user.service";
 
 @Component({
-  selector: 'app-sign-up',
-  templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+  selector: "app-sign-up",
+  templateUrl: "./sign-up.component.html",
+  styleUrls: ["./sign-up.component.css"]
 })
 export class SignUpComponent implements OnInit {
   emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   showSucessMessage: boolean;
   serverErrorMessages: string;
 
-  constructor(private userService: UserService) { }
+  constructor(public userService: UserService) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   onSubmit(form: NgForm) {
     this.userService.postUser(form.value).subscribe(
       res => {
         this.showSucessMessage = true;
-        setTimeout(() => this.showSucessMessage = false, 4000);
+        setTimeout(() => (this.showSucessMessage = false), 4000);
         this.resetForm(form);
       },
       err => {
         if (err.status === 422) {
-          this.serverErrorMessages = err.error.join('<br/>');
-        }
-        else
-          this.serverErrorMessages = 'Something went wrong.Please contact admin.';
+          this.serverErrorMessages = err.error.join("<br/>");
+        } else
+          this.serverErrorMessages =
+            "Something went wrong.Please contact admin.";
       }
     );
   }
 
   resetForm(form: NgForm) {
     this.userService.selectedUser = {
-      fullName: '',
-      email: '',
-      password: ''
+      fullName: "",
+      email: "",
+      password: ""
     };
     form.resetForm();
-    this.serverErrorMessages = '';
+    this.serverErrorMessages = "";
   }
-
 }
