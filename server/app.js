106f10eca94ce5ead9b6e69fb1befe0029312bdf
Akila_09405 <asak999@gmail.com>
2019-09-26 15:25:56 +0530

    update

--- server/app.js
+++ server/app.js
@@ -1,13 +1,14 @@
-require('./config/config');
-require('./models/db');
-require('./config/passportConfig');
+require("./config/config");
+// require("dotenv").config();
+require("./models/db");
+require("./config/passportConfig");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const passport = require('passport');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const passport = require("passport");
 
-const rtsIndex = require('./routes/index.router');
+const rtsIndex = require("./routes/index.router");
 
 var app = express();
 
@@ -15,19 +16,23 @@ var app = express();
 app.use(bodyParser.json());
 app.use(cors());
 app.use(passport.initialize());
-app.use('/api', rtsIndex);
+app.use("/api", rtsIndex);
 
 // error handler
 app.use((err, req, res, next) => {
-    if (err.name === 'ValidationError') {
-        var valErrors = [];
-        Object.keys(err.errors).forEach(key => valErrors.push(err.errors[key].message));
-        res.status(422).send(valErrors)
-    }
-    else{
-        console.log(err);
-    }
+  if (err.name === "ValidationError") {
+    var valErrors = [];
+    Object.keys(err.errors).forEach(key =>
+      valErrors.push(err.errors[key].message)
+    );
+    res.status(422).send(valErrors);
+  } else {
+    console.log(err);
+  }
 });
 
+console.log(`Environment : ${process.env.NODE_ENV}`);
 // start server
-app.listen(process.env.PORT, () => console.log(`Server started at port : ${process.env.PORT}`));
+app.listen(process.env.PORT, () =>
+  console.log(`Server started at port : ${process.env.PORT}`)
+);
